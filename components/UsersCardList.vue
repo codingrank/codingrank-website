c39fd4b22454e63ea09087d2a867ef8f9d4c44a2
Charles DZADU <dkccharlie@gmail.com>
2023-03-29 22:01:45 +0000

    :fire: Update Footer and User Card

---
+++ components/UsersCardList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="bg-black max-w-screen-2xl mx-auto">
+        <div class="flex flex-wrap justify-evenly gap-10">
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+            <UsersCard></UsersCard>
+        </div>
+    </div>
+</template>
