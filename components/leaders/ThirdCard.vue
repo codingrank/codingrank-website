3ea0be2fd51df864ce0f2c9a1028c931b82853ef
Charles DZADU <dkccharlie@gmail.com>
2023-03-29 23:08:27 +0000

    Update: Create components for home screen

---
+++ components/leaders/ThirdCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-secondary text-white p-4 w-full md:w-1/3">
+        3st Card
+    </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-black text-white">
+        <div class="max-w-screen-2xl mx-auto p-10">
+            <NavBar></NavBar>
+            <slot></slot>
+            <Footer></Footer>
+        </div>
+    </div>
+</template>
