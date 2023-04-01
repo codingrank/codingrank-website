21950e529a36f2e60b8cf19e32fabab2aeb69ffd
Djaglo Doris <dorisdjalo@gmail.com>
2023-04-01 07:11:03 +0000

    Update: Create user card

--- components/UsersCardList.vue
+++ components/UsersCardList.vue
@@ -2,13 +2,13 @@
     <div class="pt-20">
 
         <hr class="text-gray-dark">
-        <div class=" flex justify-between items-end pt-5">
+        <div class=" flex flex-col items-start gap-2 md:flex-row justify-between md:items-end pt-5">
             <div>
-                <h1 class="font-bold text-4xl py-3">Top Contributeurs au Togo</h1>
+                <h1 class="font-bold text-xl md:text-3xl lg:text-4xl py-3">Top Contributeurs au Togo</h1>
             <p>Base on contributions on public repositories</p>
             </div>
             <div>
-                <input type="text" class="h-10 w-300 bg-white text-gray-dark px-2 rounded-sm border" placeholder="Search...">
+                <input type="text" class="h-10 w-300 bg-white text-gray-dark px-2 rounded-sm border self-end" placeholder="Search...">
             </div>
         </div>
         <div class="flex flex-wrap justify-between gap-y-10 my-10">
