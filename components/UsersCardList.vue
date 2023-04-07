b6ac1cd6fa156a3f105bba2648e64d62aeab7bda
charlesdzadu <dkccharlie@gmail.com>
2023-04-07 11:15:51 +0000

    Update: Users Cards completed

--- components/UsersCardList.vue
+++ components/UsersCardList.vue
@@ -2,16 +2,17 @@
     <div class="pt-20">
 
         <hr class="text-gray-dark">
-        <div class=" flex flex-col items-start gap-2 md:flex-row justify-between md:items-end pt-5">
+        <div class=" flex flex-col items-start gap-2 sm:flex-row justify-between sm:items-center md:items-end pt-5">
             <div>
-                <h1 class="font-bold text-xl md:text-3xl lg:text-4xl py-3">Top Contributeurs au Togo</h1>
-            <p>Base on contributions on public repositories</p>
+                <h1 class="font-bold text-xl sm:text-3xl  lg:text-4xl py-3">Most active developpers</h1>
+                <p>Base on contributions on public repositories</p>
             </div>
             <div>
-                <input type="text" class="h-10 w-300 bg-white text-gray-dark px-2 rounded-sm border self-end" placeholder="Search...">
+                <input type="text" class="h-10 w-300 bg-white text-gray-dark px-2 rounded-sm border self-end"
+                    placeholder="Search...">
             </div>
         </div>
-        <div class="flex flex-wrap justify-between gap-y-10 my-10">
+        <div class="flex flex-wrap justify-evenly gap-y-10 my-10">
             <UsersCard></UsersCard>
             <UsersCard></UsersCard>
             <UsersCard></UsersCard>
@@ -26,8 +27,8 @@
             <UsersCard></UsersCard>
             <UsersCard></UsersCard>
             <UsersCard></UsersCard>
-            <UsersCard></UsersCard>
-            <UsersCard></UsersCard>
+            
+
         </div>
     </div>
 </template>
